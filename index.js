48a318818cfb95bd3ce8bd9b62c9be2dd3babd16
syzer3@gmail.com <syzer3@gmail.com>
2018-01-24 13:15:20 +0100

    * Add ability to get Http links * Fix in eslint where key was twice * Allow arrow functions

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const REGEXES = {
   users: '<@(U.*?)>',
   groups: '<!subteam\\^(S.*?)(?:\\|@.*?)>',
   files: '<(http.*.slack.com\/files\/.*\/F.*?)>',
+  // @see https://stackoverflow.com/questions/161738/what-is-the-best-regular-expression-to-check-if-a-string-is-a-valid-url
+  links: '<((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[\\-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9\\.\\-]+|(?:www\\.|[\\-;:&=\\+\\$,\\w]+@)[A-Za-z0-9\\.\\-]+)((?:\\/[\\+~%\\/\\.\\w\\-]*)?\\??(?:[\\-\\+=&;%@\\.\\w]*)#?(?:[\\.\\!\\/\\\\\\w]*))?)>',
+
 }
 
 Object.keys(REGEXES).forEach(key => {
--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ const getChannels = slackEntities.getChannels
 const getUsers = slackEntities.getUsers
 const getGroups = slackEntities.getGroups
 const getFiles = slackEntities.getFiles
+const getLinks = slackEntities.getLinks
 
 const user = {
   tag: '<@USLACKBOT>',
@@ -18,12 +19,16 @@ const group = {
   tag: '<!subteam^S0000000|@besties>',
   id: 'S0000000',
 }
-const internalFile = {
-  tag: '<https://austinprivatelibrary.slack.com/files/flip/F00000000/README.md>',
-  id: 'https://austinprivatelibrary.slack.com/files/flip/F00000000/README.md',
-}
 const html = '<HTML/>'
-const externalLink = '<https://get.slack.help/hc/en-us/articles/204399343-Sharing-links-in-Slack>'
+const internalFile = 'https://austinprivatelibrary.slack.com/files/flip/F00000000/README.md'
+const externalLink = 'https://get.slack.help/hc/en-us/articles/204399343-Sharing-links-in-Slack'
+const httpsLinkWithQuery = 'https://www.google.com/?url=has-querystring'
+
+const toTag = str => '<' + str + '>'
+const toLink = str => ({
+  id: str,
+  tag: toTag(str),
+})
 
 describe('getChannels', () => {
   it('extracts tags for channels', () => {
@@ -48,8 +53,21 @@ describe('getGroups', () => {
 })
 describe('getFiles', () => {
   it('extracts tags for internal files, snippets, and posts', () => {
-    assert.deepEqual(getFiles(`${internalFile.tag} and ${externalLink}`), [
-      internalFile,
+    assert.deepEqual(getFiles(`${toTag(internalFile)} and ${toTag(externalLink)}`), [
+      toLink(internalFile),
+    ])
+  })
+})
+
+// TODO emails 'mailto:somebody@google.com' => '<mailto:somebody@google.com|somebody@google.com>'
+// @see http://jsbin.com/eqocuh/5/edit?html,js,output
+describe('getLinks', () => {
+  it('extracts tags for links', () => {
+    assert.deepEqual(getLinks(`${toTag(internalFile)}`), [
+      toLink(internalFile),
+    ])
+    assert.deepEqual(getLinks(`Have this query string ${toTag(httpsLinkWithQuery)}`), [
+      toLink(httpsLinkWithQuery),
     ])
   })
 })
